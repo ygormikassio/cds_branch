b6b28cf2670155f26c5219fa8195014e1ad95f18
Ygor Mikassio <ygor.mikassio@gmail.com>
2024-11-25 12:39:53 -0300

    Criado funcao de exibicao de mensagem

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ def gether_data():
     return n1, n2
 
 
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
+
+    return None
+
+
 def main():
     n1, n2 = gether_data()
 
-    print(n1**n2)
+    print_message(n1, n2)
 
     return None
 
